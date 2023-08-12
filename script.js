9137a17869d53c21f04cc5b2c3987a5917275a93
Vikram Tanwar <106341310+tnwrvkrm@users.noreply.github.com>
2023-08-12 21:28:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	if(sampleObject[key] != undefined){
+	  return true;
+  }
+	else{
+	return false;
+	}
 }
 
 // Do not change the code below
